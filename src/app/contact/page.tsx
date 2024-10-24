a0b52ebc0a1cc7760eab52b92f2f5c4053c5bcaf
sameerkaleem <160517211+sameerkaleem@users.noreply.github.com>
2024-10-24 15:38:30 -0700

    Update page.tsx
    updating contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,70 +1,72 @@
 import React from 'react';
-import {  FaDiscord, FaFacebook, FaInstagram,} from 'react-icons/fa';
-import {  FaYoutube } from 'react-icons/fa6';
-import { FaPhoneSquareAlt } from "react-icons/fa";
+import { FaDiscord, FaFacebook, FaInstagram } from 'react-icons/fa';
+import { FaYoutube } from 'react-icons/fa6';
+import { FaPhoneSquareAlt } from 'react-icons/fa';
 
- const Contact = () => {
-      return (
-        <div className="md:flex md:justify-around md:text-sm
-        items-center mx:px-2 px-5 md:mt-1 md:m-0 w-full grid grid-cols-3 gap-3 py-10 mt-1
-       
-       ">
-        <h1 className=" flex justify-center items-center text-2xl font-semibold pl-2" >
-        Contact Us
-CALL/TEXT: <br/>
-(+92-313)-2254939
-
-<br></br>
-
-EMAIL: sameerkaleem@gmail.com
-<br></br>
-
-FAX: +92-313-746-6609
-<br></br>
-
-Address: North Nazimabad Karachi Pakistan
-
-Connect With Us on Social Media!
-
-        </h1>
-      
-        <div className="flex justify-center items-center text-[#f5357e] ">
-        <FaInstagram className="w-12 h-12 rounded-full p-2" />
-        <p className="text-xl font-semibold pl-2">
-        <a href="https://www.instagram.com" target="_blank">Instagram </a>
+const Contact = () => {
+  return (
+    <div className="flex flex-col items-center justify-around py-10 px-5 md:px-10 w-full">
+      {/* Contact Information */}
+      <div className="text-center md:text-left mb-10">
+        <h1 className="text-2xl font-semibold mb-5">Contact Us</h1>
+        <p className="text-base leading-relaxed">
+          CALL/TEXT: <br />
+          (+92-313)-2254939
+          <br />
+          <br />
+          EMAIL: sameerkaleem@gmail.com
+          <br />
+          <br />
+          FAX: +92-313-746-6609
+          <br />
+          <br />
+          Address: North Nazimabad Karachi Pakistan
         </p>
-     </div>
-  
-     <div className="flex justify-center items-center text-[#3674e7] ">
-        <FaFacebook className="w-12 h-12 rounded-full p-2" />
-        <p className="text-xl font-semibold pl-2">
-         <a href="https://www.facebook.com" target="_blank">Facebook</a>
-         </p>
-     </div>
-    
-     <div className="flex justify-center items-center text-[#3751e6e0] ">
-        <FaDiscord className="w-12 h-12 rounded-full p-2" />
-        <p className="text-xl font-semibold pl-2">
-        <a href="https://www.discord.com" target="_blank">Discord</a>
+        <h2 className="mt-8 text-xl font-semibold">Connect With Us on Social Media!</h2>
+      </div>
 
+      {/* Social Media Links */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 text-center">
+        <div className="flex flex-col items-center">
+          <FaInstagram className="w-12 h-12 text-[#f5357e]" />
+          <a href="https://www.instagram.com" target="_blank" className="text-lg font-semibold mt-2">
+            Instagram
+          </a>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <FaFacebook className="w-12 h-12 text-[#3674e7]" />
+          <a href="https://www.facebook.com" target="_blank" className="text-lg font-semibold mt-2">
+            Facebook
+          </a>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <FaDiscord className="w-12 h-12 text-[#3751e6]" />
+          <a href="https://www.discord.com" target="_blank" className="text-lg font-semibold mt-2">
+            Discord
+          </a>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <FaYoutube className="w-12 h-12 text-[#f53943]" />
+          <a href="https://www.youtube.com" target="_blank" className="text-lg font-semibold mt-2">
+            Youtube
+          </a>
+        </div>
+      </div>
+
+      {/* Phone Contact */}
+      <div className="flex flex-col items-center mt-10">
+        <FaPhoneSquareAlt className="w-12 h-12 text-[#333]" />
+        <p className="text-lg font-semibold mt-2">
+          <a href="tel:+92-313-22549393" className="hover:underline">
+            Contact us on: +92-313-22549393
+          </a>
         </p>
-     </div>
-
-     <div className="flex justify-center items-center text-[#f53943ee] ">
-      
-        <FaYoutube className="w-12 h-12 rounded-full p-2" />
-        <p className="text-xl font-semibold pl-2">
-        <a href="https://www.youtube.com" target="_blank">Youtube</a>
-          </p>
-     </div>
-
-     <div className="flex justify-center items-center">
-     <FaPhoneSquareAlt className="w-36 h-36 rounded-full p-2" />
-     <p className="text-xl font-semibold pl-2"> </p>
-     <a href="http://localhost:3000/contact" target="_blank">contact us on:+92-313-22549393</a>
       </div>
-      </div>
-      );
-    };
-    
-    export default Contact;
+    </div>
+  );
+};
+
+export default Contact;
